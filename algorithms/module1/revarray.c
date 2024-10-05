86110b3d39f3974a9babb730ac76553f5582ec16
Lavrov Rodion <lrd.msk@gmail.com>
2024-10-05 21:22:31 +0300

    Complete revarray.c

--- algorithms/module1/revarray.c
+++ algorithms/module1/revarray.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
 void revarray(void *base, size_t nel, size_t width) {
-    char* start = (char *)base;
-    // char* end = start + (nel - 1) * width;
-    // for (int i = 0; i < 3; i++){
-    //     // base = base + i;
-    //     printf("%zu", base + i);
-    // }
-}
+    char *start = base;
+    char *end = start + ((nel - 1) * width);
 
-int main(){
-    int array[3] = {1, 2, 3};
-    int *ptr = array;
-    revarray(ptr, 3, 6);
+    if (start < end){
+        for (size_t i = 0; i < width; i++) {
+            char preserve_start = start[i];
+            start[i] = end[i];
+            end[i] = preserve_start;
+        }
+        revarray(start + width, nel - 2, width);
+    }
 }
