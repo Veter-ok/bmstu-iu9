c9c177ec45c9a1cf1f27b0a23bd815b9f40815f4
Lavrov Rodion <lrd.msk@gmail.com>
2025-02-19 16:48:35 +0300

    Edit ranktree.c

--- AlgorithmsAndDataStructures/module3/ranktree.c
+++ AlgorithmsAndDataStructures/module3/ranktree.c
@@ -5,101 +5,156 @@
 struct Elem {
     int k;
     char *v;
-    int *span;
-    struct Elem **next;
+    int count;
+    struct Elem* parent; 
+    struct Elem* left;
+    struct Elem* right; 
 };
 
-struct Elem* InitSkipList(int m){
-    struct Elem *element = malloc(sizeof(struct Elem));
-    element->next = calloc(m, sizeof(struct Elem*));
-    element->span = calloc(m, sizeof(int));
-    element->v = NULL;
-    element->k = -1000000000;
-    return element;
+struct Elem* Minimum(struct Elem *t){
+    struct Elem *x = t;
+    while(x->left != NULL) x = x->left;
+    return x;
 }
 
-struct Elem* Succ(struct Elem* l){
-    return l->next[0];
+struct Elem* Succ(struct Elem *x){
+    if (x->right != NULL){
+        return Minimum(x->right);
+    }
+    struct Elem* y = x->parent;
+    while (y != NULL && x == y->right){
+        x = y;
+        y = y->parent;
+    }
+    return y;
 }
 
-int Rank(struct Elem* l, int m, int k){
-    if (k == -1000000000) return -1;
-    int rank = 0;
-    for (int i = m - 1; i >= 0; i--){
-        while(l->next[i] != NULL && l->next[i]->k < k){
-            rank += l->span[i];
-            l = l->next[i];
+char* SearchByRank(struct Elem *t, int count){
+    struct Elem *x = t;
+    while (x->count != count){
+        if (count < x->count){
+            x = x->left;
+        }else{
+            count -= x->count + 1;
+            x = x->right;
         }
     }
-    return rank;
-}
-
-void Skip(struct Elem* l, int m, int k, struct Elem **p){
-    for (int i = m - 1; i >= 0; i--){
-        while(l->next[i] != NULL && l->next[i]->k < k){
-            l = l->next[i];
-        }
-        p[i] = l;
-    }
-}
-
-char* Lookup(struct Elem* l, int m, int k){
-    struct Elem **p = malloc(m * sizeof(struct Elem*));
-    Skip(l, m, k, p);
-    struct Elem *x = Succ(p[0]);
-    free(p);
     return x->v;
 }
 
-void Insert(struct Elem* l, int m, int k, char* v){
-    struct Elem **p = malloc(m * sizeof(struct Elem*));
-    Skip(l, m, k, p);
-    struct Elem *x = InitSkipList(m);
-    x->k = k;
-    x->v = v;
-    int rankX = Rank(l, m, p[0]->k) + 1;
-    int r = rand() * 2;
-    int i = 0;
-    for (i = 0; i < m && r % 2 == 0; i++){
-        x->next[i] = p[i]->next[i];
-        p[i]->next[i] = x;
-        int rank = Rank(l, m, p[i]->k);
-        x->span[i] = p[i]->span[i] - (rankX - rank) + 1;
-        p[i]->span[i] = rankX - rank;
-        r /= 2;
+void Insert(struct Elem **t, int k, char *v){
+    struct Elem *y = malloc(sizeof(*y));
+    y->k = k;
+    y->v = v;
+    y->parent = NULL;
+    y->left = NULL;
+    y->right = NULL;
+    y->count = 0;
+    if (*t == NULL){
+        *t = y;
+    }else{
+        struct Elem *x = *t;
+        while (1){
+            if (k < x->k){
+                x->count++;
+                if (x->left == NULL){
+                    x->left = y;
+                    y->parent = x;
+                    break;
+                }
+                x = x->left;
+            }else{
+                if (x->right == NULL){
+                    x->right = y;
+                    y->parent = x;
+                    break;
+                }
+                x = x->right;
+            }
+        }
     }
-    for(; i < m; i++){
-        x->next[i] = NULL;
-        p[i]->span[i]++;
-    }
-    free(p);
 }
 
-void Delete(struct Elem* l, int m, int k){
-    struct Elem **p = malloc(m * sizeof(struct Elem*));
-    Skip(l, m, k, p);
-    struct Elem *x = Succ(p[0]);
-    int rankX = Rank(l, m, k);
-    int i = 0;
-    for (i = 0; i < m && p[i]->next[i] == x; i++){
-        p[i]->next[i] = x->next[i];
-        p[i]->span[i] += x->span[i] - 1;
+struct Elem* Descend(struct Elem *t, int k){
+    struct Elem *x = t;
+    while (x != NULL && x->k != k){
+        if (k < x->k){
+            x = x->left;
+        }else{
+            x = x->right;
+        }
     }
-    for (; i < m; i++){
-        p[i]->span[i]--;
+    return x;
+}
+
+char* Lookup(struct Elem *t, int k){
+    return Descend(t, k)->v;
+}
+
+void updateCount(struct Elem *t) {
+    while (t != NULL && t->parent != NULL) {
+        if (t->parent->left == t) {
+            t->parent->count--;
+        }
+        t = t->parent;
     }
-    free(x->next);
-    free(x->span);
+}
+
+void ReplaceNode(struct Elem **t, struct Elem *x, struct Elem *y){
+    if (x == *t){
+        *t = y;
+        if (y != NULL) y->parent = NULL;
+    }else{
+        struct Elem *p = x->parent;
+        if (y != NULL) y->parent = p;
+        if (p->left == x){
+            p->left = y;
+        }else{
+            p->right = y;
+        }
+    }
+}
+
+void Delete(struct Elem **t, int k){
+    struct Elem *x = Descend(*t, k);
+    if (x->left == NULL && x->right == NULL){
+        updateCount(x);
+        ReplaceNode(t, x, NULL);
+    }else if (x->left == NULL){
+        updateCount(x);
+        ReplaceNode(t, x, x->right);
+    }else if (x->right == NULL){
+        updateCount(x);
+        ReplaceNode(t, x, x->left);
+    }else{
+        struct Elem *y = Succ(x);
+        updateCount(y);
+        ReplaceNode(t, y, y->right);
+        y->count = x->count;
+        x->left->parent = y;
+        y->left = x->left;
+        if (x->right != NULL) {
+            x->right->parent = y;
+            y->right = x->right;
+        }
+        ReplaceNode(t, x, y);
+    }
     free(x->v);
     free(x);
-    free(p);
+}
+
+void FreeTree(struct Elem *t) {
+    if (t == NULL) {
+        return;
+    }
+    FreeTree(t->left);
+    FreeTree(t->right);
+    free(t->v);
+    free(t);
 }
 
 int main(){
-    int n;
-    int m = 20;
-    scanf("%d", &n);
-    struct Elem *skipList = InitSkipList(m);
+    struct Elem *binaryTree = NULL;
     char command[20] = {0};
     while (strcmp(command, "END")){
         scanf("%s", command);
@@ -107,33 +162,26 @@ int main(){
             int k;
             char *v = malloc(11 * sizeof(char));
             scanf("%d%s", &k, v);
-            Insert(skipList, m, k, v);
+            Insert(&binaryTree, k, v);
         }
         if (!strcmp(command, "LOOKUP")){
             int k;
             scanf("%d", &k);
-            char *v = Lookup(skipList, m, k);
+            char *v = Lookup(binaryTree, k);
             printf("%s\n", v);
         }
         if (!strcmp(command, "DELETE")){
             int k;
             scanf("%d", &k);
-            Delete(skipList, m, k);
+            Delete(&binaryTree, k);
         }
-        if (!strcmp(command, "RANK")){
+        if (!strcmp(command, "SEARCH")){
             int k;
             scanf("%d", &k);
-            int rank = Rank(skipList, m, k);
-            printf("%d\n", rank);
+            char *v = SearchByRank(binaryTree, k);
+            printf("%s\n", v);
         }
     }
-    while (skipList != NULL) {
-        struct Elem *copySkipList = skipList->next[0];
-        if (skipList->v != NULL) free(skipList->v);
-        free(skipList->next);
-        free(skipList->span);
-        free(skipList);
-        skipList = copySkipList;
-    }
+    FreeTree(binaryTree);
     return 0;
 }
